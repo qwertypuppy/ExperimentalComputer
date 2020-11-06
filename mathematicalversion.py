4de8609db66e72f8c9e545302428d2f9e625e33c
qwertypuppy <74007181+qwertypuppy@users.noreply.github.com>
2020-11-06 15:51:53 -0600

    Update mathematicalversion.py

--- mathematicalversion.py
+++ mathematicalversion.py
@@ -2,80 +2,68 @@
 #MATH MIGHT BE WRONG, PLEASE TELL IF IT IS
 #QUBITS ARE FLOATING POINT, NOT 0 OR 1
 #DOCUMENTATION IS IN README
-q1 = 0
-q2 = 0
-q3 = 0
 q1 = float(input("q1 position - can be floating point"))
 q2 = float(input("q2 position - can be floating point"))
 q3 = float(input("q3 position - can be floating point"))
+qubits=[q1,q2,q3]
 #E IS FLOATING POINT
 def both(bit1,bit2,e):
-  global q1
-  global q2
-  global q3
-  if bit1 == q1:
-    if bit2 == q2:
-      q3 = e*q1*q2+e*(1-q1)*(1-q2)
-    elif bit2 == q3:
-      q2 = e*q1*q3+e*(1-q1)*(1-q3)
-  elif bit1 == q2:
-    if bit2 == q1:
-      q3 = e*q1*q2+e*(1-q2)*(1-q1)
-    elif bit2 == q3:
-      q1 = e*q2*q3+e*(1-q2)*(1-q3)
-  elif bit1 == q3:
-    if bit2 == q1:
-      q2=e*q1*q3+e*(1-q3)*(1-q1)
-    elif bit2 == q2:
-      q1==e*q2*q3+e*(1-q3)*(1-q2)
+  global qubits
+  if bit1 == qubits[0]:
+    if bit2 == qubits[1]:
+      qubits[2] = e*qubits[0]*qubits[1]+e*(1-qubits[0])*(1-qubits[1])
+    elif bit2 == qubits[2]:
+      qubits[1] = e*qubits[0]*qubits[2]+e*(1-qubits[0])*(1-qubits[2])
+  elif bit1 == qubits[1]:
+    if bit2 == qubits[0]:
+      qubits[2] = e*qubits[0]*qubits[1]+e*(1-qubits[1])*(1-qubits[0])
+    elif bit2 == qubits[2]:
+      qubits[0] = e*qubits[1]*qubits[2]+e*(1-qubits[1])*(1-qubits[2])
+  elif bit1 == qubits[2]:
+    if bit2 == qubits[0]:
+      qubits[1]=e*qubits[0]*qubits[2]+e*(1-qubits[2])*(1-qubits[0])
+    elif bit2 == qubits[1]:
+      qubits[0]==e*qubits[1]*qubits[2]+e*(1-qubits[2])*(1-qubits[1])
 def setsTo(bit1,e):
-  global q1
-  global q2
-  global q3
-  if bit1 == q1:
-    q1=e
-  elif bit1 == q2:
-    q2=e
-  elif bit1 == q3:
-    q3=e
+  global qubits
+  if bit1 == qubits[0]:
+    qubits[0]=e
+  elif bit1 == qubits[1]:
+    qubits[1]=e
+  elif bit1 == qubits[2]:
+    qubits[2]=e
 def invert(bit1):
-  global q1
-  global q2
-  global q3
-  if bit1 == q1:
-    q1 = 1-q1
-  elif bit1 == q2:
-    q2 = 1-q2
-  elif bit1 == q3:
-    q3 = 1-q3
+  global qubits
+  if bit1 == qubits[0]:
+    qubits[0] = 1-qubits[0]
+  elif bit1 == qubits[1]:
+    qubits[1] = 1-qubits[1]
+  elif bit1 == qubits[2]:
+    qubits[2] = 1-qubits[2]
 def all(bit1,e):
-  global q1
-  global q2
-  global q3
-  if bit1 == q1:
-    q1=q1*q2*q3*e + (1-q1)*(1-q2)*(1-q3)*e
-  elif bit1 == q2:
-    q2=q1*q2*q3*e + (1-q1)*(1-q2)*(1-q3)*e
-  elif bit1 == q3:
-    q3=q1*q2*q3*e + (1-q1)*(1-q2)*(1-q3)*e
+  global qubits
+  if bit1 == qubits[0]:
+    qubits[0]=qubits[0]*qubits[1]*qubits[2]*e + (1-qubits[0])*(1-qubits[1])*(1-qubits[2])*e
+  elif bit1 == qubits[1]:
+    qubits[1]=qubits[0]*qubits[1]*qubits[2]*e + (1-qubits[0])*(1-qubits[1])*(1-qubits[2])*e
+  elif bit1 == qubits[2]:
+    qubits[2]=qubits[0]*qubits[1]*qubits[2]*e + (1-qubits[0])*(1-qubits[1])*(1-qubits[2])*e
 #MATH MIGHT NOT BE RIGHT-NEEDS WORK
 def unequal(bit1,bit2,e):
-  global q1
-  global q2
-  global q3
-  if bit1 == q1:
-    if bit2 == q2:
-      q3 = e*(1-q1)*q2+e*q1*(1-q2)
-    elif bit2 == q3:
-      q2 = e*(1-q1)*q3+e*q1*(1-q3)
-  elif bit1 == q2:
-    if bit2 == q1:
-      q3 = e*(1-q2)*q1+e*q2*(1-q1)
-    elif bit2 == q3:
-      q1 = e*(1-q2)*q3+e*q2*(1-q3)
-  elif bit1 == q3:
-    if bit2 == q1:
-      q2 = e*(1-q1)*q3+e*q1*(1-q3)
-    elif bit2 == q2:
-      q1 = e*(1-q3)*q1+e*q3*(1-q1)
-print(round(q1,4),round(q2,4),round(q3,4))
+  global qubits
+  if bit1 == qubits[0]:
+    if bit2 == qubits[1]:
+      qubits[2] = e*(1-qubits[0])*qubits[1]+e*qubits[0]*(1-qubits[1])
+    elif bit2 == qubits[2]:
+      qubits[1] = e*(1-qubits[0])*qubits[2]+e*qubits[0]*(1-qubits[2])
+  elif bit1 == qubits[1]:
+    if bit2 == qubits[0]:
+      qubits[2] = e*(1-qubits[1])*qubits[0]+e*qubits[1]*(1-qubits[0])
+    elif bit2 == qubits[2]:
+      qubits[0] = e*(1-qubits[1])*qubits[2]+e*qubits[1]*(1-qubits[2])
+  elif bit1 == qubits[2]:
+    if bit2 == qubits[0]:
+      qubits[1] = e*(1-qubits[0])*qubits[2]+e*qubits[0]*(1-qubits[2])
+    elif bit2 == qubits[1]:
+      qubits[0] = e*(1-qubits[2])*qubits[0]+e*qubits[2]*(1-qubits[0])
+print(round(qubits[0],4),round(qubits[1],4),round(qubits[2],4))
